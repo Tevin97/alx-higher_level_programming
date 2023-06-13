ef6899d5e7ac917b9584f332eb2f6efe4183c2de
Tevin Murimi <Tevinmurimimwenda@gmail.com>
2023-06-13 02:13:16 -0700

    9. Find the max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    else:
+        max_num = my_list[0]
+        for num in my_list(len(my_list)):
+            if my_list[num] > max_num:
+                max_num = my_list[num]
+        return max_num
