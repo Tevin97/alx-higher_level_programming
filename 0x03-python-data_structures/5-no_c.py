8e3d5ec36291031e437566b6fbd24e53fa4ec40c
Tevin Murimi <Tevinmurimimwenda@gmail.com>
2023-06-12 20:02:22 -0700

    5. Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
+
 def no_c(my_string):
-    new_string = []
+    new_string = ""
     for char in my_string:
         if char != 'c' and char != 'C':
-            new_string.append(char)
-    return "".join(new_string)
+            new_string += char
+    return new_string
