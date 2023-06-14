f8c1df4279eda12b322dc2527f531dec875bd65c
Tevin Murimi <Tevinmurimimwenda@gmail.com>
2023-06-14 14:40:24 -0700

    10. Best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    max_val = float('-inf')
+    max_key = None
+
+    for key, value in a_dictionary.items():
+        if value > max_val:
+            max_val = value
+            max_key = key
+
+    return max_key
