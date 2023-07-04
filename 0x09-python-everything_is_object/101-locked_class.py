585999e5ec422a0551a920339e6ca92a1c09e6ff
Tevin Murimi <Tevinmurimimwenda@gmail.com>
2023-07-04 14:50:40 -0700

    30. Low memory cost

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,23 +1,8 @@
 #!/usr/bin/python3
 class LockedClass:
     """
-    A class that prevents dynamically creating new instance attributes,
-    except for 'first_name'.
+    Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
     """
 
-    def __setattr__(self, name, value):
-        """
-        Override the __setattr__ method to prevent creating new instance attributes,
-        unless the attribute is 'first_name'.
-
-        Args:
-            name (str): Name of the attribute being set.
-            value (Any): Value to be assigned to the attribute.
-
-        Raises:
-            AttributeError: If trying to create a new attribute other than 'first_name'.
-        """
-        if hasattr(self, name) or name == "first_name":
-            super().__setattr__(name, value)
-        else:
-            raise AttributeError("'LockedClass' object has no attribute '{}'".format(name))
+    __slots__ = ["first_name"]
