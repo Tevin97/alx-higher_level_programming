4ec078d1300f2ef34f0878ce08cdd243616837fc
Tevin Murimi <Tevinmurimimwenda@gmail.com>
2023-07-05 18:57:50 -0700

    0. Simple rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+
+"""
+This module defines a Rectangle class that represents a rectangle.
+
+Attributes:
+    No attributes are defined yet.
+"""
+
 class Rectangle:
     """
     This class represents a rectangle.
