a553affef6f94b0a5a52319d86d1006f83310445
Tevin Murimi <Tevinmurimimwenda@gmail.com>
2023-07-12 14:24:58 -0700

    0. Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines a text file-reading function."""
+
+def read_file(filename=""):
+    """
+    Reads the contents of a file and prints it to the console.
+
+    Args:
+        filename (str): The name of the file to be read.
+
+    Returns:
+        None
+
+    Raises:
+        FileNotFoundError: If the specified file does not exist.
+    """
+    
+    try:
+        with open(filename, encoding="utf-8") as t:
+            print(t.read(), end="")
+
+    except FileNotFoundError:
+        print(f"File '{filename}' not found.")
