5cb400d18e8267f0bf677aa388c485778083aaf2
Tevin <tevinmurimimwenda@gmail.com>
2023-09-12 04:56:42 -0700

    4. Create a sentence

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const a = ' is ';
-let firstArg = process.argv[2];
-let secondArg = process.argv[3];
-let finalSentence = firstArg + a + secondArg;
+const firstArg = process.argv[2];
+const secondArg = process.argv[3];
+const finalSentence = firstArg + a + secondArg;
 console.log(finalSentence);
