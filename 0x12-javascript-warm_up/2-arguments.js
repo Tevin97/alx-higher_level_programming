ce3258474b82e1a4e2d5407d6daa857194dc7e63
Tevin <tevinmurimimwenda@gmail.com>
2023-09-12 04:19:53 -0700

    2. Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 const count = process.argv.length;
 if (count < 3) {
-	console.log('No argument');
+  console.log('No argument');
+}else if (count === 3){
+  console.log('Argument found');
 } else {
-	console.log('Argument found');
+  console.log('Argument found');
 }
