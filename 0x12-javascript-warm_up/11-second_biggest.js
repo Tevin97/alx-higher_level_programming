a5166399c6307be61c53bac330331674ea306175
Tevin <tevinmurimimwenda@gmail.com>
2023-09-12 10:01:54 -0700

    11. Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 const argv = process.argv;
 if (!argv[2] || !argv[3]) {
   console.log(0);
-} else { 
+} else {
   const sortedArgv = argv.sort((a, b) => a - b);
   console.log(sortedArgv[sortedArgv.length - 2]);
 }
