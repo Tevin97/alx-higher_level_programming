a90926c5fc304e324b1ba78ae89b91fc17e8447b
Tevin <tevinmurimimwenda@gmail.com>
2023-10-24 17:50:46 -0700

    0. Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 const filePath = process.argv[2];
 
-fs.readFile(filePath, 'utf-8', (err, content) => {
+fs.readFile(filePath, 'utf-8', function (err, content) {
   if (err) {
     console.error(err);
   } else {
