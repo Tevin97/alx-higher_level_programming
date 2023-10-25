42b39f664d9746f10e8dc5bd153e10c5b4966561
Tevin <tevinmurimimwenda@gmail.com>
2023-10-24 18:11:22 -0700

    4. Star wars Wedge Antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = process.argv[2];
+const characterId = 18;
+
+request.get(apiUrl, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const films = JSON.parse(body).results;
+    const moviesWithWedgeAntilles = films.filter(film => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`));
+    console.log('Number of movies with Wedge Antilles:', moviesWithWedgeAntilles.length);
+  }
+});
